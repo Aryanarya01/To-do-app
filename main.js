a12085494e438114c84995ea2a4db51fa35727d5
Aryanarya01 <aryanarya2777@gmial.com>
2025-06-27 23:22:05 +0530

    initial commit

--- main.js
+++ main.js
@@ -1,34 +1,32 @@
-
-//                                      search university by name of state        
-
-
- let url = "http://universities.hipolabs.com/search?name=";
-
- let btn = document.querySelector("button");
  
- btn.addEventListener("click", async () => {
-     let query = document.querySelector("input").value;
-     console.log("Search query:", query); 
-     let colArr = await getColleges(query);
-     show(colArr);
- });
- 
- async function getColleges(query) {
-     try {
-         let res = await axios.get(url + query);
-         return res.data;
-     } catch (err) {
-         console.log("Error:", err);
-     }
- }
- 
- function show(colArr) {
-     let list = document.querySelector("#list");
-     list.innerText = "";
-     for (let col of colArr) {
-         console.log(col.name, col["state-province"]);
-         let li = document.createElement("li");
-         li.innerText = `${col.name} - ${col["state-province"] || "N/A"}`;
-         list.appendChild(li);
-     }
- }
+//                                 todo activity
+
+let btn = document.querySelector("button");
+let inp = document.querySelector("input");
+let ul = document.querySelector("ul");
+
+btn.addEventListener("click",function(){
+
+    let item = document.createElement("li");
+    item.innerText = inp.value;
+
+
+    let delbtn = document.createElement("button");
+    delbtn.innerText="delete";
+    delbtn.classList.add("delete");
+
+
+    item.appendChild(delbtn);
+    ul.appendChild(item);   
+    inp.value="";   
+});
+
+
+ul.addEventListener("click",function(event){
+    if(event.target.nodeName=="BUTTON"){
+        let listItem = event.target.parentElement;
+        listItem.remove();
+        console.log("deleted");
+    }
+   
+}); 
